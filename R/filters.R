33cb3f0dee94a06fb9cbcd41864a4a41975f1b3f
kgori <kevin.c.gori@gmail.com>
2018-09-18 10:33:12 +0100

    Fix bug in 02_assign_germline.R and filters.R; switch to RDS datatype

--- R/filters.R
+++ R/filters.R
@@ -1,10 +1,21 @@
 #' @export
-variant_depth_filter <- function(dt, column_names, min_variant_reads, min_vaf) {
+variant_depth_filter <- function(dt, variant_column_names, min_variant_reads, min_vaf) {
     mask <- rep(FALSE, nrow(dt))
-    for (col in column_names) {
-        mask <- mask | dt[, eval(as.name(col)) >= min_variant_reads & eval(as.name(sub(".nr", ".vaf", col))) >= min_vaf]
+    if (length(min_variant_reads) == length(variant_column_names)) {
+        thresholds <- data.frame(name = variant_column_names,
+                                 threshold = min_variant_reads,
+                                 stringsAsFactors = FALSE)
+    } else {
+        thresholds <- data.frame(name = variant_column_names,
+                                 threshold = rep(min_variant_reads[1], length(variant_column_names)),
+                                 stringsAsFactors = FALSE)
+    }
+    for (i in 1:nrow(thresholds)) {
+        nv <- thresholds[i, ][["name"]]
+        vaf <- sub(".nv", ".vaf", nv)
+        min_nv <- thresholds[i, ][["threshold"]]
+        mask <- mask | dt[, eval(as.name(nv)) >= min_nv | eval(as.name(vaf)) >= min_vaf]
     }
     mask[is.na(mask)] <- FALSE
     mask
 }
-
