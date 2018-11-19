9da983a7b0815af9bbdf78e10a7da23135acb0c1
kgori <kevin.c.gori@gmail.com>
2018-11-19 14:39:57 +0000

    Refactor plotting

--- R/mixture_model.R
+++ R/mixture_model.R
@@ -57,6 +57,71 @@ max_relerror <- function(va, vb) {
     max(numer / denom)
 }
 
+#' Plot a single iteration of the mixture model estimation procedure of `estimate_mixture`
+#' @importFrom "grid" popViewport pushViewport grid.draw
+#' @importFrom "gridExtra" tableGrob ttheme_minimal
+#' @importFrom "gridBase" baseViewports
+#' @param "data" The observations being fitted
+#' @param "params" Current Beta component `a` and `b` parameters
+#' @param "pi" Component mixture weights
+#' @param "step" Current iteration number
+plot_estimation_step <- function(data, params, pi, step) {
+    # Calculate likelihood of data given parameters
+    weights_ll <- get_weights(data, params, pi)
+    weights <- weights_ll$weights
+    ll <- weights_ll$ll
+
+    # Calculate data for line of uncertainty, and the boundaries
+    x <- seq(0, 1, length.out = 1001)
+    w <- get_weights(x, params, pi)$weights
+    uncertainty <- 1 - apply(w, 1, max)
+    bounds <- c(
+        beta_boundary(params[1, 1], params[1, 2], params[2, 1], params[2, 2], pi[1], pi[2], log = FALSE),
+        beta_boundary(params[2, 1], params[2, 2], params[3, 1], params[3, 2], pi[2], pi[3], log = FALSE)
+    )
+
+    # Draw the histogram
+    layout(matrix(c(1,1,1,3,
+                    1,1,1,1,
+                    2,2,2,2), nrow = 3, byrow = TRUE))
+    par(mar = c(-0.1, 4, 4, 2) + 0.1)
+    hist(data,
+         breaks = seq(0, 1, length.out = 51),
+         border = "white",
+         col = hcl(240, 50, 80),
+         probability = TRUE,
+         main = paste("step", step, "ll =", round(ll, 4)),
+         #sub = paste(round(pi, 2), collapse = " "),
+         xaxt = "n", xlab = NA)
+
+    # Add lines for the component densities
+    dens <- t(t(apply(params, 1, function(r) dbeta(x, r[1], r[2]))) * pi)
+    apply(dens, 2, function(c) lines(x, c, lty = 3, col = hcl(240, 80, 50)))
+    lines(x, rowSums(dens), col = hcl(240, 100, 30), lwd = 2, lty = 1)
+
+    # Add boundaries
+    abline(v = bounds, col = "red")
+
+    # Plot uncertainty
+    par(mar = c(5, 4, -0.1, 2) + 0.1)
+    plot(x, uncertainty, ylim = c(0, 1), type = "l", bty = "n", xlab = "VAF", ylab = "Uncertainty")
+    abline(v = bounds, col = "red")
+
+    # Draw table of parameters
+    df <- data.frame(a = params[, 1], b = params[, 2], pi = pi)
+    frame()
+    # Grid regions of current base plot (ie from frame)
+    vps <- baseViewports()
+    pushViewport(vps$inner, vps$figure, vps$plot)
+    mytheme <- ttheme_minimal(
+        core = list(fg_params=list(cex = 0.7)),
+        colhead = list(fg_params=list(cex = 0.6)),
+        rowhead = list(fg_params=list(cex = 0.6)))
+    grob <- tableGrob(round(df, 2), theme = mytheme)
+    grid.draw(grob)
+    popViewport(3)
+}
+
 #' Run Expectation Maximisation to optimise the fit of a mixture of Beta components
 #' to data
 #' @param data (numeric matrix) - Observations in the range [0,1]
@@ -77,7 +142,7 @@ estimate_mixture <- function(data, init_params, init_pi,
     n <- length(data)
     ncomp <- nrow(init_params)
 
-    if (!sum(init_pi) == 1) stop("init_pi must sum to 1")
+    if (!all.equal.numeric(sum(init_pi), 1)) stop(paste0("init_pi must sum to 1: (", paste(init_pi, collapse=", "), ")"))
     if (any(init_params <= 0)) stop("init_params must all be > 0")
     if (!nrow(init_params) == length(init_pi)) stop("Number of rows of init_params must equal length of init_pi")
 
@@ -86,12 +151,7 @@ estimate_mixture <- function(data, init_params, init_pi,
     pi <- init_pi
 
     if (plot) {
-        ll<-get_weights(data,params,pi)$ll
-        hist(data, breaks = 100, border = "white", col = hcl(240, 50, 80), probability = TRUE, main = paste("step", step, "ll =", round(ll, 4)), sub = paste(round(pi, 2), collapse = " "))
-        x <- seq(0, 1, length.out = 1001)
-        dens <- t(t(apply(params, 1, function(r) dbeta(x, r[1], r[2]))) * pi)
-        apply(dens, 2, function(c) lines(x, c, lty = 3, col = hcl(240, 80, 50)))
-        lines(x, rowSums(dens), col = hcl(240, 100, 30), lwd = 2, lty = 1)
+        plot_estimation_step(data, params, pi, step)
     }
 
     for (i in 1:max_steps) {
@@ -110,11 +170,7 @@ estimate_mixture <- function(data, init_params, init_pi,
         if (step %% 10 == 0) print(paste(step, delta))
 
         if (plot) {
-            hist(data, breaks = 100, border = "white", col = hcl(240, 50, 80), probability = TRUE, main = paste("step", step, "ll =", round(w$ll, 4)), sub = paste(round(pi, 2), collapse = " "))
-            x <- seq(0, 1, length.out = 1001)
-            dens <- t(t(apply(params, 1, function(r) dbeta(x, r[1], r[2]))) * pi)
-            apply(dens, 2, function(c) lines(x, c, lty = 3, col = hcl(240, 80, 50)))
-            lines(x, rowSums(dens), col = hcl(240, 100, 30), lwd = 2, lty = 1)
+            plot_estimation_step(data, params, pi, step)
         }
         if (delta < tol) {
             break
@@ -139,16 +195,18 @@ estimate_mixture <- function(data, init_params, init_pi,
          data = data, ll = w$ll, bic = bic)
 }
 
+#' Get initial parameter estimates for the mixture model
 #' @param v Model data
 #' @param k Number of clusters
 #' @return (List) Initial estimates of params and pi
+#' @export
 get_init <- function(v, k = 3) {
     params <- matrix(0, ncol = 2, nrow = k)
     pi <- rep(0, k)
     bounds <- c(0, 1:k/k)
     for (i in 1:k) {
         dat <- v[v>bounds[i] & v <= bounds[i+1]]
-        params[i, ] <- beta_ab(mean(dat), var(dat))
+        params[i, ] <- beta_ab(mean(dat, na.rm = TRUE), var(dat, na.rm = TRUE))
         pi[i] <- length(dat) / length(v)
     }
     pi <- pi / sum(pi)
