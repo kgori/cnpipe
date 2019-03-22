5775be528da0bcaf5a63f363be14fdbcece661d7
kgori <kevin.c.gori@gmail.com>
2019-03-22 18:39:21 +0000

    Symmetrical and reflected beta distributions

---
+++ R/reflected_beta.R
@@ -0,0 +1,56 @@
+# Density, density-derivative, log-likelihood and derivative-log-likelihood functions
+# for a Beta distribution reflected at 0.5
+
+#' Reflected beta distribution density function
+#' @export
+dreflbeta <- function(x, a, b, log = FALSE) {
+    d1 <- dbeta(x, a, b, log = log)
+    d2 <- dbeta(x, b, a, log = log)
+    if (log) {
+        densm <- matrix(c(d1, d2), ncol = 2)
+        return(apply(densm, 1, function(r) {
+            m <- max(r)
+            log(sum(exp(r - m))) + m
+        }))
+    }
+    d1 + d2
+}
+
+dreflbeta_deriv_wrt_x <- function(x, a, b) {
+    numer1 <- x^(a - 1) * (1 - b) * (1 - x)^(b - 1)
+    numer2 <- x^(a - 1) * (1 - a) * (1 - x)^(b - 1)
+    numer3 <- x^(b - 1) * (1 - a) * (1 - x)^(a - 1)
+    numer4 <- x^(b - 1) * (1 - b) * (1 - x)^(a - 1)
+    denom1 <- (1 - x) * beta(a, b)
+    denom2 <- x * beta(a, b)
+    numer1 / denom1 - numer2 / denom2 + numer3 / denom1 - numer4 / denom2
+}
+
+dreflbeta_deriv_wrt_a <- function(x, a, b) {
+    elem1 <- x^(a - 1) * (1 - x)^(b - 1) * (log(x) + digamma(a + b) - digamma(a))
+    elem2 <- x^(b - 1) * (1 - x)^(a - 1) * (log(1 - x) + digamma(a + b) - digamma(a))
+    return((elem1 + elem2) / beta(a, b))
+}
+
+dreflbeta_deriv_wrt_b <- function(x, a, b) {
+    elem1 <- x^(a - 1) * (1 - x)^(b - 1) * (log(1 - x) - digamma(b) + digamma(a + b))
+    elem2 <- x^(b - 1) * (1 - x)^(a - 1) * (log(x) - digamma(b) + digamma(a + b))
+    return((elem1 + elem2) / beta(a, b))
+}
+
+#' Log-likelihood of a sample conditioned on these parameters, assuming reflected beta model
+#' @export
+ll_reflbeta <- function(params, data) {
+    sum(dreflbeta(data, params[1], params[2], log = TRUE))
+}
+
+dll_reflbeta <- function(params, data) {
+    ll <- dreflbeta(data, params[1], params[2], log = TRUE)
+    mll <- max(ll)
+    dll_wrt_a <- dreflbeta_deriv_wrt_a(data, params[1], params[2])
+    dll_wrt_b <- dreflbeta_deriv_wrt_b(data, params[1], params[2])
+
+
+    c(sum(dll_wrt_a / exp(ll - mll) / exp(mll)),
+      sum(dll_wrt_b / exp(ll - mll) / exp(mll)))
+}
---
+++ R/symmetrical_beta_mixture.R
@@ -0,0 +1,114 @@
+# Functions describing a mixture of two Beta distributions with symmetrical parameters,
+# i.e. 0.5 * Beta(A, B) + 0.5 * Beta(B, A)
+
+#' Density function for symmetrical beta mixture
+#' @export
+dsbeta <- function(x, a, b, w, log = FALSE) {
+    if (log) {
+        d <- data.frame(e1=log(w) + dbeta(x, a, b, log = TRUE),
+                        e2=log(1-w) + dbeta(x, b, a, log = TRUE))
+        # Mixture of two densities is done elementwise, using logSumExp
+        return(apply(d, 1, function(r) {
+            m <- max(r)
+            log(sum(exp(r - m))) + m
+        }))
+    }
+    # Mixture of two densities
+    w * dbeta(x, a, b) + (1 - w) * dbeta(x, b, a)
+}
+
+#' PDF function for symmetrical beta mixture
+#' @export
+psbeta <- function(q, a, b, w) {
+    w * pbeta(q, a, b) + (1 - w) * pbeta(q, b, a)
+}
+
+#' Quantile (CDF) function for symmetrical beta mixture
+#' @export
+qsbeta <- function(p, a, b, w) {
+    G <- function(x) psbeta(x, a, b, w) - p
+    return (uniroot(G, c(0, 1))$root)
+}
+
+#' Random variates function for symmetrical beta mixture
+#' @export
+rsbeta <- function(n, a, b, w) {
+    c(rbeta(n*w, a, b), rbeta(n*(1-w), b, a))
+}
+
+
+# Derivatives of the likelihood
+dsbeta_deriv_wrt_x <- function(x, a, b, w) {
+    numer1 <- w * x^(a - 1) * (1 - b) * (1 - x)^(b - 1)
+    numer2 <- w * x^(a - 1) * (1 - a) * (1 - x)^(b - 1)
+    numer3 <- (1 - w) * x^(b - 1) * (1 - a) * (1 - x)^(a - 1)
+    numer4 <- (1 - w) * x^(b - 1) * (1 - b) * (1 - x)^(a - 1)
+    denom1 <- (1 - x) * beta(a, b)
+    denom2 <- x * beta(a, b)
+    numer1 / denom1 - numer2 / denom2 + numer3 / denom1 - numer4 / denom2
+}
+
+dsbeta_deriv_wrt_a <- function(x, a, b, w) {
+    elem1 <- w * x^(a - 1) * (1 - x)^(b - 1) * (log(x) + psigamma(a + b) - psigamma(a))
+    elem2 <- (1 - w) * x^(b - 1) * (1 - x)^(a - 1) * (log(1 - x) + psigamma(a + b) - psigamma(a))
+    return((elem1 + elem2) / beta(a, b))
+}
+
+dsbeta_deriv_wrt_b <- function(x, a, b, w) {
+    elem1 <- w * x^(a - 1) * (1 - x)^(b - 1) * (log(1 - x) - psigamma(b) + psigamma(a + b))
+    elem2 <- (1 - w) * x^(b - 1) * (1 - x)^(a - 1) * (log(x) - psigamma(b) + psigamma(a + b))
+    return((elem1 + elem2) / beta(a, b))
+}
+
+dsbeta_deriv_wrt_w <- function(x, a, b, w) {
+    elem1 <- x^(a - 1) * (1 - x)^(b - 1)
+    elem2 <- x^(b - 1) * (1 - x)^(a - 1)
+    return((elem1 - elem2) / beta(a, b))
+}
+
+
+#' Log-likelihood of a sample conditioned on these parameters, assuming symmetrical beta mixture model
+#' @export
+llsbeta <- function(xs, a, b, w) {
+    sum(dsbeta(xs, a, b, w, log = TRUE))
+}
+
+llsbeta_deriv_wrt_a <- function(xs, a, b, w) {
+    sum(dsbeta_deriv_wrt_a(xs, a, b, w) / dsbeta(xs, a, b, w))
+}
+
+llsbeta_deriv_wrt_b <- function(xs, a, b, w) {
+    sum(dsbeta_deriv_wrt_b(xs, a, b, w) / dsbeta(xs, a, b, w))
+}
+
+llsbeta_deriv_wrt_w <- function(xs, a, b, w) {
+    sum(dsbeta_deriv_wrt_w(xs, a, b, w) / dsbeta(xs, a, b, w))
+}
+
+dbeta_deriv_wrt_x <- function(x, a, b) {
+    betafn_ab <- beta(a, b)
+    numer1 <- x^(a - 1) * (1 - b) * (1 - x)^(b - 1)
+    denom1 <- (1 - x) * betafn_ab
+    numer2 <- x^(a - 1) * (a - 1) * (1 - x)^(b - 1)
+    denom2 <- x * betafn_ab
+    (numer1 / denom1) + (numer2 / denom2)
+}
+
+dbeta_deriv_wrt_a <- function(x, a, b) {
+    (x^(a - 1) * (1 - x)^(b - 1) * (log(x) + psigamma(a + b) - psigamma(a))) / beta(a, b)
+}
+
+dbeta_deriv_wrt_b <- function(x, a, b) {
+    (x^(a - 1) * (1 - x)^(b - 1) * (log(1 - x) + psigamma(a + b) - psigamma(b))) / beta(a, b)
+}
+
+
+finite_diff <- function(fn, params, param_index, h = 0.0001) {
+    params_1 <- params
+    params_2 <- params
+    for (i in param_index) {
+        params_1[[i]] <- params_1[[i]] - h
+        params_2[[i]] <- params_2[[i]] + h
+    }
+    (do.call(fn, params_2) - do.call(fn, params_1)) / (2*h)
+}
